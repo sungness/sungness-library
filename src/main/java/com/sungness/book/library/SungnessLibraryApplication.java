3cdee87ad41237f1836162c13cd5149b036d79ee
sungness <sungness@gmail.com>
2021-05-20 23:07:06 +0800

    Project initialize.

---
+++ src/main/java/com/sungness/book/library/SungnessLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.sungness.book.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SungnessLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SungnessLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sungness/book/library/SungnessLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sungness.book.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SungnessLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
